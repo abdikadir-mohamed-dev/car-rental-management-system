5f7dc23efb224b7fe6f63461b5c5c9f25d59d537
ccgo14 <calebmuuo64@gmail.com>
2026-08-18 16:59:57 +0300

    chore: initialize React build setup and base dependencies

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
