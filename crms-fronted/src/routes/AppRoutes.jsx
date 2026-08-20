6b9ecdf357b02f554b5a5341b88d6b5dc1406381
risper njeri <rispernjeri789@gmail.com>
2026-08-20 23:37:36 +0300

    chore(customer): remove non-shared admin/staff/driver/public/notification files and clean routes

--- crms-fronted/src/routes/AppRoutes.jsx
+++ crms-fronted/src/routes/AppRoutes.jsx
@@ -2,12 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import PublicRoutes from './PublicRoutes'
 import AuthRoutes from './AuthRoutes'
 import CustomerRoutes from './CustomerRoutes'
-import AdminRoutes from './AdminRoutes'
-import StaffRoutes from './StaffRoutes'
-import DriverRoutes from './DriverRoutes'
-import ProtectedRoute from '../components/common/ProtectedRoute'
 import ErrorBoundary from '../components/common/ErrorBoundary'
-import { USER_ROLES } from '../utils/constants'
 
 function AppRoutes() {
   return (
@@ -17,21 +12,6 @@ function AppRoutes() {
         <ErrorBoundary>
           <CustomerRoutes />
         </ErrorBoundary>
-      } />
-      <Route path="/admin/*" element={
-        <ProtectedRoute allowedRoles={[USER_ROLES.ADMIN]}>
-          <AdminRoutes />
-        </ProtectedRoute>
-      } />
-      <Route path="/staff/*" element={
-        <ProtectedRoute allowedRoles={[USER_ROLES.STAFF]}>
-          <StaffRoutes />
-        </ProtectedRoute>
-      } />
-      <Route path="/driver/*" element={
-        <ProtectedRoute allowedRoles={[USER_ROLES.DRIVER]}>
-          <DriverRoutes />
-        </ProtectedRoute>
       } />
       <Route path="/*" element={<PublicRoutes />} />
       <Route path="*" element={<Navigate to="/" replace />} />
