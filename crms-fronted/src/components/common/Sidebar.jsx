3582cf28083979d49edc15f567bfb9c222576446
Emmanuel Sum <esumkipkorir@gmail.com>
2026-08-20 20:13:04 +0300

    fix sidebar to remain fixed on scroll across all layouts

--- crms-fronted/src/components/common/Sidebar.jsx
+++ crms-fronted/src/components/common/Sidebar.jsx
@@ -70,7 +70,7 @@ function Sidebar({ isOpen, onClose, role }) {
         onClick={onClose}
       />
       <aside
-        className={`fixed top-0 left-0 z-50 h-full w-64 bg-sidebar text-white transform transition-transform lg:translate-x-0 lg:static lg:z-auto ${
+        className={`fixed top-0 left-0 z-50 h-full w-64 bg-sidebar text-white transform transition-transform ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
--- crms-fronted/src/layouts/AdminLayout.jsx
+++ crms-fronted/src/layouts/AdminLayout.jsx
@@ -9,7 +9,7 @@ function AdminLayout() {
   return (
     <div className="min-h-screen flex bg-background">
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} role="admin" />
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 lg:ml-64">
         <Header onMenuClick={() => setSidebarOpen(true)} />
         <main className="flex-1 p-6">
           <Outlet />
