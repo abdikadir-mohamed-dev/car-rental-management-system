3d0a074e745f75453bcbcc5d1c254f3e1ccef299
abdikadir-mohamed-dev <abdikadirmohammed180@gmail.com>
2026-09-04 09:33:17 +0300

    Configure frontend API URL for deployment

--- crms-fronted/src/utils/constants.js
+++ crms-fronted/src/utils/constants.js
@@ -34,4 +34,5 @@ export const TRIP_STATUS = {
   CANCELLED: 'cancelled',
 }
 
-export const VITE_API_URL = 'http://localhost:5000'
+export const VITE_API_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:5000'
